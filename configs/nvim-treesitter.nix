3a93391dfef330885e2ea64ac49ee7d967523b16
me <me>
2024-03-17 03:57:40 +0100

    configs: Extend treesitter

---
+++ configs/nvim-treesitter.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, nix2nvimrc, ... }:
+{
+  # extend ck3d
+  configs.nvim-treesitter = {
+    plugins = with pkgs.vimPlugins; [
+      rainbow-delimiters-nvim
+    ];
+    setup.args = {
+      rainbow = {
+        enable = true;
+        extended_mode = true;
+      };
+    };
+  };
+}
